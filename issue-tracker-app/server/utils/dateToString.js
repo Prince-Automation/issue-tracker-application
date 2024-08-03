184fc892becc61357fb6cd11bd102820c95b0343
Prince-Automation <87142711+Prince-Automation@users.noreply.github.com>
2024-08-02 20:18:51 -0400

    Delete issue-tracker-app directory

--- issue-tracker-app/server/utils/dateToString.js
+++
@@ -1,5 +0,0 @@
-const dateToString = (date) => {
-    return new Date(date).toLocaleDateString("sv-SE");
-};
-
-module.exports = dateToString;
--- issue-tracker-app/server/utils/stringToDate.js
+++
@@ -1,9 +0,0 @@
-const stringToDate = (string) => {
-    const parsedValue = new Date(string);
-    if (!isNaN(parsedValue.getTime())) {
-        return parsedValue;
-    }
-    return "";
-};
-
-module.exports = stringToDate;
